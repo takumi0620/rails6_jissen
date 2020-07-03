871856a538c1f7c72b4de3e113aa537f5c42dd92
山本卓実 <yamamototakumi@koumotoukeinoMacBook-Pro.local>
2020-07-03 23:23:19 +0900

    chapter5完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  layout :set_layout # レイアウトを行うためのメソッドを指定
+
+  private def set_layout
+    # paramsオブジェクトを返却
+    # params[:controller]で現在選択されているコントローラの名前を取得できる。
+    if params[:controller].match(%r{\A(staff|admin|customer)/})
+      Regexp.last_match[1]
+    else
+      "customer"
+    end
+  end
 end
