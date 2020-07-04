4d059f18209206707ca1adf39b6c08d2a4a86c93
山本卓実 <yamamototakumi@koumotoukeinoMacBook-Pro.local>
2020-07-04 19:09:05 +0900

    chapter6完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   layout :set_layout # レイアウトを行うためのメソッドを指定
 
+  class Forbidden < ActionController::ActionControllerError; end
+  class IpAddressRejected < ActionController::ActionControllerError; end
+
+  include ErrorHandlers if Rails.env.production?
+
   private def set_layout
     # paramsオブジェクトを返却
     # params[:controller]で現在選択されているコントローラの名前を取得できる。
