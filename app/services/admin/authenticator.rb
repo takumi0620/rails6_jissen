74c15af0ee05de0e162946c4242566d7d138d73c
山本卓実 <yamamototakumi@koumotoukeinoMacBook-Pro.local>
2020-07-15 01:51:46 +0900

    chapter8演習完了

---
+++ app/services/admin/authenticator.rb
@@ -0,0 +1,11 @@
+class Admin::Authenticator
+  def initialize(admin_member)
+    @admin_member = admin_member
+  end
+
+  def authenticate(raw_password)
+    @admin_member &&
+    @admin_member.hashed_password &&
+    BCrypt::Password.new(@admin_member.hashed_password) == raw_password
+  end
+end
