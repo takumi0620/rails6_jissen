e447179b72bdf0f590c8411685647cf9099bb80e
山本卓実 <yamamototakumi@koumotoukeinoMacBook-Pro.local>
2020-08-16 16:59:57 +0900

    chapter17完了

--- app/models/concerns/string_normalizer.rb
+++ app/models/concerns/string_normalizer.rb
@@ -15,4 +15,8 @@ module StringNormalizer
   def normalize_as_furigana(text)
     NKF.nkf("-W -w -Z1 --katakana", text).strip if text
   end
+
+  def normalize_as_postal_code(text)
+    NKF.nkf("-W -w -Z1", text).strip.gsub(/-/, "") if text
+  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,13 +1,14 @@
 class Customer < ApplicationRecord
+  include EmailHolder
+  include PersonalNameHolder
+  include PasswordHolder
   # has_oneはモデル間に1:1の関連付けを設定する
-  has_one :home_address, dependent: :destroy
-  has_one :work_address, dependent: :destroy
-
-  def password=(raw_password)
-    if raw_password.kind_of?(String)
-      self.hashed_password = BCrypt::Password.create(raw_password)
-    elsif raw_password.nil?
-      self.hashed_password = nil
-    end
-  end
+  has_one :home_address, dependent: :destroy, autosave: true
+  has_one :work_address, dependent: :destroy, autosave: true
+  validates :gender, inclusion: { in: %w(male female), allow_blank: true }
+  validates :birthday, date: {
+    after: Date.new(1900, 1, 1),
+    before: ->(obj) { Date.today },
+    allow_blank: true
+  }
 end
